bb79757384e9adeec5016f3902336ba211389045
gtolontop <cruckstore+github@gmail.com>
2026-04-21 01:47:06 +0200

    feat(logging): add LogCategory enum

---
+++ plugins/core-paper/src/main/java/fr/smp/core/logging/LogCategory.java
@@ -0,0 +1,6 @@
+package fr.smp.core.logging;
+
+public enum LogCategory {
+    CHAT, JOIN, COMBAT, DEATH, ECONOMY, SHOP, SELL, AUCTION,
+    TEAM, HOME, WARP, RTP, TPA, ADMIN, COMMAND, ERROR, SPAWNER
+}
