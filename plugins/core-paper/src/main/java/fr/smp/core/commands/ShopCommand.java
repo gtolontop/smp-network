153868f74e0dc16ef1699628fbcea1ada0ced56f
gtolontop <cruckstore+github@gmail.com>
2026-04-26 18:01:57 +0200

    refactor(core-paper): shop command improvements

--- plugins/core-paper/src/main/java/fr/smp/core/commands/ShopCommand.java
+++ plugins/core-paper/src/main/java/fr/smp/core/commands/ShopCommand.java
@@ -2,6 +2,7 @@ package fr.smp.core.commands;
 
 import fr.smp.core.SMPCore;
 import fr.smp.core.gui.ShopGUI;
+import fr.smp.core.utils.Msg;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,7 +18,44 @@ public class ShopCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player p)) return true;
+        if (args.length >= 1 && sender.hasPermission("smp.admin")) {
+            String sub = args[0].toLowerCase();
+            switch (sub) {
+                case "on", "enable" -> {
+                    plugin.shop().setEnabled(true);
+                    sender.sendMessage(Msg.ok("<green>Shop activé.</green>"));
+                    return true;
+                }
+                case "off", "disable" -> {
+                    plugin.shop().setEnabled(false);
+                    sender.sendMessage(Msg.ok("<green>Shop désactivé pour les joueurs.</green>"));
+                    return true;
+                }
+                case "toggle" -> {
+                    boolean now = !plugin.shop().isEnabled();
+                    plugin.shop().setEnabled(now);
+                    sender.sendMessage(Msg.ok("<green>Shop "
+                            + (now ? "activé" : "désactivé") + ".</green>"));
+                    return true;
+                }
+                case "reload" -> {
+                    plugin.shop().load();
+                    sender.sendMessage(Msg.ok("<green>Shop rechargé.</green>"));
+                    return true;
+                }
+            }
+        }
+
+        if (!(sender instanceof Player p)) {
+            sender.sendMessage(Msg.err("Commande réservée aux joueurs."));
+            return true;
+        }
+
+        if (!plugin.shop().isEnabled() && !p.hasPermission("smp.admin")) {
+            p.sendMessage(Msg.err("Le shop est désactivé."));
+            return true;
+        }
+
         new ShopGUI(plugin).open(p);
         return true;
     }
