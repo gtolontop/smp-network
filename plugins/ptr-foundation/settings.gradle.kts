4d14818494d95234d671f392fef48b3ed51a0d10
gtolontop <cruckstore+github@gmail.com>
2026-05-10 23:57:48 +0200

    feat(ptr-foundation): bootstrap Gradle build (paperweight, shadow, spotless, checkstyle, JUnit5)

---
+++ plugins/ptr-foundation/settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+}
+
+rootProject.name = "ptr-foundation"
